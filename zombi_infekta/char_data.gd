aa25e2ddaf965affa07ae668c8a0657958023da3
FUZZY-PC-1\Lucas <kyuubinx@gmail.com>
2024-04-02 23:29:39 -0300

    [LS-3] Atualização dos dados de PCs e NPCs

---
+++ zombi_infekta/char_data.gd
@@ -0,0 +1,52 @@
+extends Node
+
+var luke_data = {
+	"name":"Luke",
+	"sur":"Shone",
+	"hp":18,
+	"st":7,
+	"dmg":2,
+	"san":7,
+	"luc":10,
+	"acc":3,
+	"ill":"SCHIZOPHRENIA",
+	"ill_desc":"Display can get confused."
+}
+
+var valentine_data = {
+	"name":"Valentine",
+	"sur":"Martin",
+	"hp":14,
+	"st":10,
+	"dmg":1,
+	"san":8,
+	"luc":12,
+	"acc":5,
+	"ill":"BORDERLINE",
+	"ill_desc":"Attacks whenever it's possible."
+}
+
+var bart_data = {
+	"name":"Bart",
+	"sur":"Martin",
+	"hp":26,
+	"st":4,
+	"dmg":2,
+	"san":6,
+	"luc":8,
+	"acc":2,
+	"ill":"HYPOCHONDRIAC",
+	"ill_desc":"Consumes healing items whenever it's possible."
+}
+
+var zombie_data = {
+	"hp":5,
+	"dmg":3,
+	"acc":3
+}
+
+var szombie_data = {
+	"hp":7,
+	"dmg":5,
+	"acc":4
+}
