8a3da1309c46d1466a381740d21ad01e8494d00f
Gustavo Henrique Werle <gustavohenrique6224@gmail.com>
2024-04-07 04:29:36 -0300

    Versão porca do randomizador
    inserção da variável de zumbis, de mapas de exemplo para sortear os zumbis, e um for para sortear a quantidade de zumbis de acordo com o mapa e inserir na variável de zumbis

--- zombi_infekta/randomizer.gd
+++ zombi_infekta/randomizer.gd
@@ -1,3 +1,9 @@
 extends Node2D
 
 var array_zombies = [0,0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,4,4,5]
+var zombies = []
+var maps_to_draw = [1,5,7,8]
+func draw():
+	for i in maps_to_draw:
+		var zombie = array_zombies.pick_random()
+		zombies = [[i, zombie]]
